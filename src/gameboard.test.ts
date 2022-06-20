dcd111c60cfca700d6b0cd25a7036781d6f7110d
Andrew <abysmalduck@gmail.com>
2022-06-19 21:01:55 -0700

    Cant get ship sinking check to work, will need to refactor

--- src/gameboard.test.ts
+++ src/gameboard.test.ts
@@ -19,13 +19,59 @@ test("Builds board", () => {
 test("places horizontal ship", () => {
   const board = new Gameboard();
   board.placeShip(2, true, 0, 0);
-  expect(board.board[0][0]).toBe(2);
-  expect(board.board[0][1]).toBe(2);
+  expect(board.board[0][0]).toBe(1);
+  expect(board.board[0][1]).toBe(1);
 });
 
 test("places vertical ship", () => {
   const board = new Gameboard();
   board.placeShip(2, false, 0, 0);
-  expect(board.board[0][0]).toBe(2);
-  expect(board.board[1][0]).toBe(2);
+  expect(board.board[0][0]).toBe(1);
+  expect(board.board[1][0]).toBe(1);
+});
+
+test("Attack misses", () => {
+  const board = new Gameboard();
+  expect(board.receiveAttack(0, 0)).toBe(2);
+});
+
+test("Attack hits", () => {
+  const board = new Gameboard();
+  board.placeShip(2, false, 0, 0);
+  expect(board.receiveAttack(0, 0)).toBe(3);
+});
+
+test("Attack invalid", () => {
+  const board = new Gameboard();
+  board.placeShip(2, false, 0, 0);
+  board.receiveAttack(0, 0);
+  expect(board.receiveAttack(0, 0)).toBe(0);
+});
+
+test("All ships not sunk", () => {
+  const board = new Gameboard();
+  board.placeShip(2, true, 0, 0);
+  expect(board.checkAllSunk()).toBe(false);
+});
+
+test("All ships sunk", () => {
+  const board = new Gameboard();
+  board.placeShip(2, true, 0, 0);
+  board.receiveAttack(0, 0);
+  board.receiveAttack(1, 0);
+  expect(board.shipCoords).toEqual([]);
+  expect(board.checkAllSunk()).toBe(true);
+});
+
+test("Filter Array", () => {
+  const board = new Gameboard();
+  expect(
+    board.filterArray(
+      [
+        [0, 0],
+        [1, 0],
+      ],
+      [0, 0]
+    )
+  ).toBe([[1, 0]]);
 });
--- src/gameboard.ts
+++ src/gameboard.ts
@@ -1,29 +1,64 @@
 export class Gameboard {
   board: number[][];
+  shipCoords: number[][];
+
   constructor() {
     this.board = this.buildBoard();
+    this.shipCoords = [];
   }
   // Gameboard legend:
   // "0" is an empty space
-  // "1" has a miss
-  // "2" has a ship on it
+  // "1" has a ship on it
+  // "2" has a miss
   // "3" has a hit (and therefore a ship)
 
   private buildBoard() {
     return new Array(10).fill(new Array(10).fill(0));
   }
 
-  placeShip(size: number, horizontal: boolean, array: number, index: number) {
+  placeShip(size: number, horizontal: boolean, y: number, x: number) {
+    // Sets game board values, and returns a list of coordinates
     if (horizontal) {
       for (let i = 0; i < size; i++) {
-        this.board[array][index + i] = 2;
+        this.shipCoords.push([x + i, y]);
+        this.board[y][x + i] = 1;
       }
     } else {
       for (let i = 0; i < size; i++) {
-        this.board[array + 1][index] = 2;
+        this.shipCoords.push([x, y + 1]);
+        this.board[y + 1][x] = 1;
       }
     }
   }
 
-  receiveAttack(x: number, y: number) {}
+  private changeValue(x: number, y: number, value: number) {
+    this.board[y][x] = value;
+  }
+
+  private removeCoords(x: number, y: number) {
+    this.shipCoords = this.filterArray(this.shipCoords, [x, y]);
+  }
+
+  filterArray(array: number[][], value: number[]) {
+    return array.filter((f) => f[0] != value[0] && f[1] != value[1]);
+  }
+
+  receiveAttack(x: number, y: number) {
+    // Take in coordinates, return number code for new space result
+    if (this.board[y][x] > 1) {
+      this.changeValue(x, y, 0);
+      return 0;
+    } else if (this.board[y][x] === 1) {
+      this.changeValue(x, y, 3);
+      this.removeCoords(x, y);
+      return 3;
+    } else if (this.board[y][x] === 0) {
+      this.changeValue(x, y, 2);
+      return 2;
+    }
+  }
+
+  checkAllSunk() {
+    return this.shipCoords.length === 0;
+  }
 }
